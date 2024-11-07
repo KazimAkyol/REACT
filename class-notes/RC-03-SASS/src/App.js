78e35f51c637ecb43453445f5063b62e723af126
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-07 09:33:34 +0100

    third commit

--- class-notes/RC-03-SASS/src/App.js
+++ class-notes/RC-03-SASS/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
+import "./App.scss";
+import Card from "./components/card/Card";
+import dataA from "./data";
+import Header from "./components/header/Header";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  // console.log(dataA);
+
+  return (
+    <div>
+      <Header />
+
+      <Card veri={dataA} />
+    </div>
+  );
+};
 
 export default App;
