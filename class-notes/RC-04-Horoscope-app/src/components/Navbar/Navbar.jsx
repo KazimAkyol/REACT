f14e26f2bcf0f5e41a8c8496c75d844f5b87981e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-08 10:20:20 +0100

    12.commit

--- class-notes/RC-04-Horoscope-app/src/components/Navbar/Navbar.jsx
+++ class-notes/RC-04-Horoscope-app/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+
+import logo from "../../helper/logo.png";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+        <div>
+            <img src={logo} width="100px" alt="" />
+        </div>
+
+        <div className='link'>
+            <a href="#horoscope">Horoscope</a>
+            <a href="#daily">Daily</a>
+            <a href="#tarot">Tarot</a>
+            <a href="#article">Article</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </div>
   )
 }
 
