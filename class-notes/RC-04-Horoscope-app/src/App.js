77f170bbb7483514aa14ecee3b933842dac695a9
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-11 10:39:40 +0100

    19.commit

--- class-notes/RC-04-Horoscope-app/src/App.js
+++ class-notes/RC-04-Horoscope-app/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./App.scss";
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
-
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
 
-        <Header/>
+      <Header />
+
+      <Main />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
