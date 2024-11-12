82724bf39b9fc95af67e2605226ae716faa018fe
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-12 12:23:53 +0100

    32.commit

--- class-notes/RC-05-06-Events-Hooks/src/2-classComponent-state/Counter.jsx
+++ class-notes/RC-05-06-Events-Hooks/src/2-classComponent-state/Counter.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { Component } from "react";
 
-const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+//* ====================== CLASS COMPONENTS AND STATE ======================
+//* React taki Class-Components  ES6 class yapısını temel alır.
+//* çok fazla boilerplate code a sahipler.
+//* fakat, state lere sahip olabilirler
+//*  state component hakkında data veya bilgi içermek için kullanılan yerleşik bir React object tidir. component in durumu zamanla değişebilir ;
+//* ne zaman değişse component re-render olur.
+//* this.state ile constructor da state için bir initial value atayabiliriz. constructor  dışında, state i setState() metoduyla değiştirebiliriz,
+//* =========================================================================
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    //! state'e baslangic degerleri atadik
+    this.state = {
+      sayac: 0,
+      baslik: "merhaba",
+      kisi: { isim: "muhterem", meslek: "developer" },
+    };
+  }
+
+  arttir() {
+    this.setState({ sayac: this.state.sayac + 1 });
+
+    this.setState({ baslik: "HALLO" });
+  }
+
+  //!burada arrow function tercih ettiğimiz için button kısmında fonksiyonu bind etmeye gerek kalmadı.
+  azalt = () => {
+    this.setState({ sayac: this.state.sayac - 1 });
+  };
+
+  render() {
+    return (
+      <div className="text-center">
+        <h1>SAYAC: {this.state.sayac} </h1>
+        <h2>{this.state.baslik}</h2>
+
+        <button onClick={this.arttir.bind(this)} className="btn btn-primary">
+          ARTTIR
+        </button>
+
+        <button onClick={this.azalt} className="btn btn-primary">
+          AZALT
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Counter
+export default Counter;
