f3ea5b6ef117282403d36a94af21fb599253dd6a
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-12 09:20:08 +0100

    26.commit

---
+++ class-notes/RC-05-06-Events-Hooks/src/App.js
@@ -0,0 +1,11 @@
+import Events from "./1-events-hooksintro/Events";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div>
+    <Events/>
+  </div>;
+}
+
+export default App;
