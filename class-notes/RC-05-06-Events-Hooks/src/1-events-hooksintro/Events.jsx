dd8135ba60f79a1132db5fc0d6938f18c7d72b38
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-12 09:46:08 +0100

    27.commit

--- class-notes/RC-05-06-Events-Hooks/src/1-events-hooksintro/Events.jsx
+++ class-notes/RC-05-06-Events-Hooks/src/1-events-hooksintro/Events.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Events = () => {
-  return (
-    <div>Events</div>
-  )
-}
+  //! JavaScript alani
 
-export default Events
+  let baslik = "MERHABA";
+  let sayac = 0;
+
+  const arttir = () => {
+    sayac = sayac + 1;
+
+    console.log(sayac);
+
+    document.querySelector("span").textContent = sayac;
+  };
+
+  const changeBaslik = (title) => {
+    baslik = title;
+    console.log(baslik);
+
+    document.querySelector("h1").textContent = title;
+  };
+
+  return (
+    //! jsx(react) alani
+
+    <div className="container text-center mt-4">
+      <h1>INFO:{baslik}</h1>
+
+      <h2>
+        COUNT: <span className="text-danger">{sayac}</span>
+      </h2>
+
+      <button onClick={arttir} className="btn btn-primary">
+        ARTTIR
+      </button>
+      <button onClick={() => changeBaslik("HALLO")} className="btn btn-info">
+        BASLIK DEGISTIR
+      </button>
+      <button className="btn btn-danger">TIKLANDI</button>
+    </div>
+  );
+};
+
+export default Events;
