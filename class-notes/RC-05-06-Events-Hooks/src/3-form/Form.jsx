b97ec3cda72047e0970a520ca1493eb4b55aa3ea
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-13 10:14:31 +0100

    37.commit

--- class-notes/RC-05-06-Events-Hooks/src/3-form/Form.jsx
+++ class-notes/RC-05-06-Events-Hooks/src/3-form/Form.jsx
@@ -9,40 +9,60 @@ import React, { useState } from "react";
 //! Ayrıca JSX'de  javascript ifadeleri {} içinde çalıştırılabilir. Buna map, && gibi yapılarda dahil.
 
 const Form = () => {
+  const [isim, setIsim] = useState("");
+  const [pass, setPass] = useState("");
+  const [country, setCountry] = useState("");
 
-    const[isim, setIsim] = useState("")
-    const[pass, setPass] = useState("")
-    const[] = useState("")
+  const gonderDatabase = (e) => {
+    e.preventDefault();
+    //? ileride bir submit islemin neticesinde verilerin nasıl post edilebileceğini gostermek adina eklenmistir.
+    //? const res = axios.post("url", { isim, pass, country })
+    alert(`name:${isim}  password: ${pass} country : ${country}`);
+  };
+
   return (
     <div className="container text-center mt-4">
       <h1>*********************************************</h1>
 
       <h1>FORM(EVENTS)</h1>
 
-      <form>
+      <form onSubmit={gonderDatabase}>
         <div className="mb-3">
           <label className="form-label" htmlFor="isim">
             NAME: <span className="text-danger">{isim}</span>
           </label>
-          <input className="form-control" id="isim" type="text" onInput={(e)=>setIsim(e.target.value)} />
+          <input
+            className="form-control"
+            id="isim"
+            type="text"
+            onInput={(e) => setIsim(e.target.value)}
+          />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="password">
-            PASSWORD
+            PASSWORD : {pass}
           </label>
-          <input className="form-control" id="password" type="password" onChange={(p)=> setPass(p.target.value)} />
+          <input
+            className="form-control"
+            id="password"
+            type="password"
+            onChange={(p) => setPass(p.target.value)}
+          />
         </div>
 
         <div className="mb-3">
           <label className="form-label" htmlFor="country">
-            COUNTRY: <span className="text-danger"> </span>
+            COUNTRY: <span className="text-danger">{country}</span>
           </label>
-          <select className="form-select">
+          <select
+            className="form-select"
+            onChange={(c) => setCountry(c.target.value)}
+          >
             <option selected>COUNTRIES</option>
             <option value="Türkiye">TURKEY</option>
             <option value="Almanya">GERMANY</option>
             <option value="Amerika">USA</option>
-          </select onChange{(c)=>}>
+          </select>
         </div>
         <button type="submit" className="btn btn-primary">
           GÖNDER (submit)
