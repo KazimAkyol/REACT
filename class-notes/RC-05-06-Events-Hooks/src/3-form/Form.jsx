e512a08e7dd2fb0ad6ed329f3e770678caad5f55
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-13 09:47:34 +0100

    36.commit

--- class-notes/RC-05-06-Events-Hooks/src/3-form/Form.jsx
+++ class-notes/RC-05-06-Events-Hooks/src/3-form/Form.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+
+//! JSX ile HTML arasında çok fazla fark olmamakla birlikte bazı önemli farklar da mevcut elbette.
+//! HTML'de class olarak tanımlanan nitelik JSX'de className olarak tanımlanmalı.
+//! HTML'de tek kelime olmayan tabindex gibi nitelikler JSX'de camelCase yapısına uygun olarak tabIndex şeklinde dönüştürülür.
+//! HTML'de onclick, onchange gibi nitelikler yukarıdaki camelCase örneğine göre onClick, onChange şeklinde dönüştürülür.
+//! HTML'de label etiketinde belirtilen for attribute ü JSX'de htmlFor olarak tanımlanmalı.
+//! HTML'de value niteliği JSX'de defaultValue olarak tanımlanmalı.
+//! Ayrıca JSX'de  javascript ifadeleri {} içinde çalıştırılabilir. Buna map, && gibi yapılarda dahil.
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
 
-export default Form
+    const[isim, setIsim] = useState("")
+    const[pass, setPass] = useState("")
+    const[] = useState("")
+  return (
+    <div className="container text-center mt-4">
+      <h1>*********************************************</h1>
+
+      <h1>FORM(EVENTS)</h1>
+
+      <form>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="isim">
+            NAME: <span className="text-danger">{isim}</span>
+          </label>
+          <input className="form-control" id="isim" type="text" onInput={(e)=>setIsim(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            PASSWORD
+          </label>
+          <input className="form-control" id="password" type="password" onChange={(p)=> setPass(p.target.value)} />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="country">
+            COUNTRY: <span className="text-danger"> </span>
+          </label>
+          <select className="form-select">
+            <option selected>COUNTRIES</option>
+            <option value="Türkiye">TURKEY</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select onChange{(c)=>}>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          GÖNDER (submit)
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
