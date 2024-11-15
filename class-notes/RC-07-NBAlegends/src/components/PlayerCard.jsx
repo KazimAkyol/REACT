a5b7b6104e236ebab69690a46bd45b9867c0de31
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-15 11:45:37 +0100

    45.commit

--- class-notes/RC-07-NBAlegends/src/components/PlayerCard.jsx
+++ class-notes/RC-07-NBAlegends/src/components/PlayerCard.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 
 const PlayerCard = ({ name, img, statistics }) => {
+  const [showResim, setShow] = useState(true);
+
   console.log(statistics);
   return (
-    <Card style={{ width: "18rem" }} className="player-kart">
-      <Card.Img variant="top" src={img} className="player-resim" />
+    <Card
+      style={{ width: "18rem" }}
+      className="player-kart m-auto"
+      role="button"
+      onClick={() => setShow(!showResim)}
+    >
+      {showResim ? (
+        <Card.Img variant="top" src={img} className="player-resim" />
+      ) : (
+        <ul>
+          {statistics.map((item, index) => (
+            <li className="h3 list-unstyled text-start"> 🏀 {item} </li>
+          ))}
+        </ul>
+      )}
+
       <Card.Footer className="text-muted">
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
