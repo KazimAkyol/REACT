d2885b22c7c0897a85340c540bef97e93ebe3a60
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-15 09:37:46 +0100

    41.commit

--- class-notes/RC-07-NBAlegends/src/App.js
+++ class-notes/RC-07-NBAlegends/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import CardContainer from "./components/CardContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
