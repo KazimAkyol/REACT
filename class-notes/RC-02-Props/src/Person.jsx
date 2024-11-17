49ae9311ad463b94e7d30dd2366ba38915b37fa1
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-17 15:13:04 +0100

    48.commit

--- class-notes/RC-02-Props/src/Person.jsx
+++ class-notes/RC-02-Props/src/Person.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import Message from "./Message";
 
 const Person = (props) => {
-  // console.log(props)
-  // console.log(props,kisi.name)
+  // console.log(props);
+  // console.log(props.kisi.name);
 
   // destructuring
   const { name, img, tel } = props.kisi;
 
   return (
     <div>
-      <h1> {name} </h1>
+      <h1>{name}</h1>
 
       <img src={img} alt="" />
 
-      <p> {tel} </p>
+      <p>{tel}</p>
 
       {/* <Message isim="Osman Kara" /> */}
       <Message isim={name} telefon={tel} />
--- class-notes/RC-03-SASS/src/components/card/Card.jsx
+++ class-notes/RC-03-SASS/src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ veri }) => {
   // console.log(props);
   console.log(veri);
-  //! react alanında (ekrana basarken) döngü olarak sadece map desteklenir,condition lardan da sadece ternary desteklenir
+  //! react alanında (ekrana basarken) döngü olarak sadece map desteklenir,condition'lardan da sadece ternary desteklenir.
 
   //!map döngüsü kullanılan her projede map ten sonraki ilk etikete key={id} yazılır
 
@@ -11,9 +11,9 @@ const Card = ({ veri }) => {
     <div>
       {veri.map((a) => (
         <div key={a.id}>
-          <h1> {a.name}</h1>
-          <h2>{a.job} </h2>
-          <p>{a.comment} </p>
+          <h1>{a.name}</h1>
+          <h2>{a.job}</h2>
+          <p>{a.comment}</p>
           <img src={a.img} alt="user" />
           <div>
             <button className="buton-div--small">Small</button>
