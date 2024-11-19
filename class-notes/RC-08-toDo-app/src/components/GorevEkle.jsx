b8e96a23a4036745580788720f587a9cbbb9ee66
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-19 10:38:14 +0100

    61.commit

--- class-notes/RC-08-toDo-app/src/components/GorevEkle.jsx
+++ class-notes/RC-08-toDo-app/src/components/GorevEkle.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const GorevEkle = () => {
-  return (
-    <div>GorevEkle</div>
-  )
-}
+  const [todo, setTodo] = useState("");
+  const [day, setDay] = useState("");
 
-export default GorevEkle
+  const gonder = (e) => {
+    e.preventDefault()
+
+    
+  }
+
+  return (
+    <div>
+      <header className="header">
+        <h1>TO DO APP</h1>
+        <button className="btn" style={{ background: "red" }}>
+          CLOSE ADD TASK BAR
+        </button>
+      </header>
+
+      <form onSubmit={gonder}>
+        <div className="form-control">
+          <label htmlFor="text">Task</label>
+          <input
+            id="text"
+            type="text"
+            name="text"
+            onChange={(e) => setTodo(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day & Time</label>
+          <input
+            id="day"
+            type="datetime-local"
+            name="day"
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className="btn btn-submit" type="submit">
+            SUBMİT
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default GorevEkle;
