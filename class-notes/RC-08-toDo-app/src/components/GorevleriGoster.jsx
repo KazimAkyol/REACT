de85907c6ec9562fc7c7978cee98d023c9a955e7
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-19 12:25:31 +0100

    64.commit

--- class-notes/RC-08-toDo-app/src/components/GorevleriGoster.jsx
+++ class-notes/RC-08-toDo-app/src/components/GorevleriGoster.jsx
@@ -5,7 +5,17 @@ const GorevleriGoster = ({ yapilacak, setYapilacak }) => {
   return (
     <div>
       {yapilacak.map((a) => (
-        <div key={a.id} className={a.isDone === true ? "done" : "gorev"}>
+        <div
+          key={a.id}
+          className={a.isDone === true ? "done" : "gorev"}
+          onDoubleClick={() =>
+            setYapilacak(
+              yapilacak.map((dizi) =>
+                dizi.id === a.id ? { ...dizi, isDone: !dizi.isDone } : dizi
+              )
+            )
+          }
+        >
           <h3>
             {a.text}
 
