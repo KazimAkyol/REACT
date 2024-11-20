74fab487ad80dfd8814301a38ca190dc1052ea57
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-20 10:25:56 +0100

    66.commit

---
+++ class-notes/RC-09-Appointment-App/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HastaEkle from '../components/HastaEkle'
+
+const Home = () => {
+  return (
+    <div>
+        <HastaEkle />
+    </div>
+  )
+}
+
+export default Home
