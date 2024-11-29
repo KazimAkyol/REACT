00e53c93325ba65bb23fad6a15c944f34c9d3f81
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-29 09:45:34 +0100

    115.commit

--- class-notes/RC-12-13-reactRouter/src/pages/TeacherDetails.jsx
+++ class-notes/RC-12-13-reactRouter/src/pages/TeacherDetails.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const TeacherDetails = () => {
-  return (
-    <div>TeacherDetails</div>
-  )
-}
+  //! 1.yol navigate yolladığı path e id yi gömerek yolladı, burada da useParams la id yi çektik*******
 
-export default TeacherDetails
+  //   const { id } = useParams();
+  // path ile yollanan id yi useParams hook uyla yakaladık
+  //   const [kisi, setKisi] = useState({});
+
+  //   useEffect(() => {
+  //     axios
+  //       .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+  //       .then((res) => setKisi(res.data))
+  //   }, [id]);
+
+  //! 2.yol
+  //! useLocation.//  navigate ile gonderilen state'i yakalamak icin useLocation Hook'u kullanilabilir.
+  //! Bu durumda veri, state ile geldigi icin yeniden fetch yapilmasina gerek kalmaz
+  const {
+    state: { kisi },
+  } = useLocation();
+  return (
+    <div>
+      <img
+        src={`https://api.dicebear.com/9.x/avataaars/svg?seed=${kisi.id}`}
+        alt=""
+      />
+      <h3>{kisi.name}</h3>
+      <h2>{kisi.email}</h2>
+      <h5>{kisi.website}</h5>
+      <h5>{kisi.phone}</h5>
+    </div>
+  );
+};
+
+export default TeacherDetails;
