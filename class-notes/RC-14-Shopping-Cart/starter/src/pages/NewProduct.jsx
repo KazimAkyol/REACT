062733ee8cfbd0b6d3be03f3e9e91f74fd94a3e7
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-02 14:53:38 +0100

    127.commit

--- class-notes/RC-14-Shopping-Cart/starter/src/pages/NewProduct.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/pages/NewProduct.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewProduct = () => {
-  return <div className="container"></div>;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [image, setImage] = useState("");
+
+  const gonderDatabase = (e) => {
+    e.preventDefault();
+
+    alert(
+      `name:${name}  price: ${price} quantity: ${quantity} image:${image} `
+    );
+
+    setName("");
+    setPrice("");
+    setQuantity("");
+    setImage("");
+  };
+
+  return (
+    <div className="container text-center mt-4">
+      <h1>New Product</h1>
+
+      <form onSubmit={gonderDatabase}>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="isim">
+            Product Name <span className="text-danger">{name} </span>
+          </label>
+          <input
+            className="form-control"
+            id="isim"
+            type="text"
+            onInput={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            Product Price {price}
+          </label>
+          <input
+            className="form-control"
+            id="password"
+            type="password"
+            value={price}
+            onChange={(p) => setPrice(p.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            Product Quantity {quantity}
+          </label>
+          <input
+            className="form-control"
+            id="password"
+            type="password"
+            value={quantity}
+            onChange={(p) => setQuantity(p.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            Product Image {image}
+          </label>
+          <input
+            className="form-control"
+            id="password"
+            type="password"
+            value={image}
+            onChange={(p) => setImage(p.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Save To New Product
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default NewProduct;
