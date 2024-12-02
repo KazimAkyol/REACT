10397c689733670791dbfa972b879aa6c3891637
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-02 13:56:57 +0100

    125.commit

--- class-notes/RC-14-Shopping-Cart/starter/src/components/MyNavbar.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/components/MyNavbar.jsx
@@ -1,46 +1,24 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const MyNavbar = () => {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Shopping Cart
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <Navbar expand="lg" className="bg-body-aqua">
+      <Container>
+        <Navbar.Brand href="#home">Shopping Cart</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link color="red" href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">New Product</Nav.Link>
+            <Nav.Link href="#link">Product List</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
