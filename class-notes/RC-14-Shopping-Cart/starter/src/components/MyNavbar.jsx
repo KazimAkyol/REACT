118de45c822cd78e06138d324429e1bfb8a4052f
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-02 17:05:29 +0100

    130.commit

--- class-notes/RC-14-Shopping-Cart/starter/src/components/MyNavbar.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/components/MyNavbar.jsx
@@ -2,27 +2,28 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 const MyNavbar = () => {
   return (
     <Navbar expand="lg" className="bg-body-primary">
       <Container>
-        <Navbar.Brand href="#home">Shopping Cart</Navbar.Brand>
+        <Navbar.Brand>Shopping Cart</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="nav-link" to="/">
+            <NavLink className="nav-link" to="/">
               Home
-            </Nav.Link>
-            <Nav.Link className="nav-link" to="/newProduct">
+            </NavLink>
+            <NavLink className="nav-link" to="/newProduct">
               New Product
-            </Nav.Link>
-            <Nav.Link className="nav-link" to="/productList">
+            </NavLink>
+            <NavLink className="nav-link" to="/productList">
               Product List
-            </Nav.Link>
-            <Nav.Link className="nav-link" to="/about">
+            </NavLink>
+            <NavLink className="nav-link" to="/about">
               About
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
