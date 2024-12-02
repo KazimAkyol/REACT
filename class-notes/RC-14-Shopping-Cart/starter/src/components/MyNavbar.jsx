92bf435fea6bc536ff45280855630d8ad771bd88
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-02 14:35:47 +0100

    126.commit

--- class-notes/RC-14-Shopping-Cart/starter/src/components/MyNavbar.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/components/MyNavbar.jsx
@@ -5,13 +5,15 @@ import Navbar from "react-bootstrap/Navbar";
 
 const MyNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-aqua">
+    <Navbar expand="lg" className="bg-body-primary">
       <Container>
         <Navbar.Brand href="#home">Shopping Cart</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link color="red" href="#home">Home</Nav.Link>
+            <Nav.Link color="red" href="#home">
+              Home
+            </Nav.Link>
             <Nav.Link href="#link">New Product</Nav.Link>
             <Nav.Link href="#link">Product List</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
