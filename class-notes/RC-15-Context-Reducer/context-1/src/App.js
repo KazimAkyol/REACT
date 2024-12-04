30c67410423c57a6502569f7b65deca6426e74a3
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-04 09:41:58 +0100

    137.commit

--- class-notes/RC-15-Context-Reducer/context-1/src/App.js
+++ class-notes/RC-15-Context-Reducer/context-1/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { createContext, useState } from "react";
+import Home from "./components/Home";
+import data from "./data";
+
+//! 1-context alani aciyoruz
+export const StudentContext = createContext();
 
 const App = () => {
+  const [students, setStudents] = useState(data);
   return (
-    <div>App</div>
-  )
-}
+    //! 2-bütün projeye gönderilebilsin diye ilk olarak Home sayfasini gönderilecek verilerle ve contextle sarmallayalim
 
-export default App
+    <StudentContext.Provider value={{ students }}>
+      <Home />
+    </StudentContext.Provider>
+  );
+};
+
+export default App;
