8e2bab514ba1366cb57db2e6998d4126b5384a81
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-05 11:54:10 +0100

    149.commit

--- class-notes/RC-16-Styled-Component/Styled-Components-2/src/components/Card.jsx
+++ class-notes/RC-16-Styled-Component/Styled-Components-2/src/components/Card.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import data from "../data";
+import ImageSSS from "./styles/ImageSSS";
 
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div>
+      {data.map((a) => (
+        <div key={a.id}>
+          <div>
+            <h2>{a.title}</h2>
+            <p></p>
+          </div>
 
-export default Card
+          <ImageSSS src={`/images/${a.image}`} alt="" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Card;
