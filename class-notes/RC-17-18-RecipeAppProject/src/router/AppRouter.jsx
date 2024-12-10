c813fa8fe7b6a1b0508474ca4dd8306e29afd961
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-10 22:31:26 +0100

    163.commit

--- class-notes/RC-17-18-RecipeAppProject/src/router/AppRouter.jsx
+++ class-notes/RC-17-18-RecipeAppProject/src/router/AppRouter.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/login/Login";
 
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import Login from "../pages/login/Login";
 import PrivateRouter from "./PrivateRouter";
-import Details from "../pages/details/Details";
-import Home from "./../pages/home/Home";
-import NotFound from "./../pages/errorPage/NotFound";
-import About from "../pages/about/About";
+import Home from "../pages/home/Home";
 
 const AppRouter = () => {
   return (
@@ -18,16 +15,10 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Login />} />
 
-        <Route path="/about" element={<About />} />
-
         {/* şifre kontrolü için private router'i gönderme */}
         <Route path="/home" element={<PrivateRouter />}>
           <Route path="" element={<Home />} />
         </Route>
-
-        <Route path="/details" element={<Details />} />
-
-        <Route path="*" element={<NotFound />} />
       </Routes>
 
       <Footer />
