b6402de62e5aeff9660c012fd99d42b1bc86fb48
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-11 12:17:46 +0100

    170.commit

--- class-notes/RC-18-Tailwind/src/App.js
+++ class-notes/RC-18-Tailwind/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import HoverFocus from "./components/HoverFocus";
+import Navbar from "./components/Navbar";
 import Responsive from "./components/Responsive";
 import Sizing from "./components/Sizing";
 import Typography from "./components/Typography";
@@ -13,7 +14,9 @@ function App() {
 
       {/* <HoverFocus /> */}
 
-      <Responsive />
+      {/* <Responsive /> */}
+
+      <Navbar />
     </div>
   );
 }
