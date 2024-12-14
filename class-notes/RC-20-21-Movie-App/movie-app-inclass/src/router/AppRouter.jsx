f8c8730a11a5a15146435f0bf2a5bc08ebae3361
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-14 14:33:25 +0100

    192.commit

--- class-notes/RC-20-21-Movie-App/movie-app-inclass/src/router/AppRouter.jsx
+++ class-notes/RC-20-21-Movie-App/movie-app-inclass/src/router/AppRouter.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Register from "../pages/Register";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Main from "../pages/Main";
 import Login from "../pages/Login";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
