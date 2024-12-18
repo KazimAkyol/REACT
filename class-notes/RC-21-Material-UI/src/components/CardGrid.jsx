2d89381b9cc806b4b377bb536def09cb243a3496
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-18 12:01:49 +0100

    219.commit

--- class-notes/RC-21-Material-UI/src/components/CardGrid.jsx
+++ class-notes/RC-21-Material-UI/src/components/CardGrid.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 const CardGrid = () => {
+  const [products, setProducts] = useState([]);
 
-    fetch("https://dummyjson.com/products").then((res)=>res.json()).then((data)=>console.log(data))
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  });
 
   return (
-    <div>
+    <Container>
+      <Typography variant="h4" align="center" color="error">
+        CARD-GRID
+      </Typography>
+      {/* spacing={2} card lar arası 2 birim boşluk */}
+      <Grid container spacing={2}>
+        {products.map((a) => (
+          <Grid key={a.id} item xs={6} md={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia sx={{ height: 140 }} image={a.images} title="urun" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {a.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {a.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained" size="small">
+                  Share
+                </Button>
+                <Button variant="outlined" size="small">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
-    </div>
-  )
-}
-
-export default CardGrid
+export default CardGrid;
