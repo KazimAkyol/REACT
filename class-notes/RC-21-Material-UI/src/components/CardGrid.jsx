084c4a63a23a3ecf3af5028bedb079da24c2b0cc
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-18 11:43:42 +0100

    218.commit

--- class-notes/RC-21-Material-UI/src/components/CardGrid.jsx
+++ class-notes/RC-21-Material-UI/src/components/CardGrid.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const CardGrid = () => {
+
+    fetch("https://dummyjson.com/products").then((res)=>res.json()).then((data)=>console.log(data))
+
   return (
-    <div>CardGrid</div>
+    <div>
+
+    </div>
   )
 }
 
