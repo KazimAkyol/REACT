0c942c1b1601682887d05d6e30351ec5514e4db8
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-23 11:15:32 +0100

    229.commit

--- class-notes/RC-24-redux-toolkit-example/src/router/AppRouter.jsx
+++ class-notes/RC-24-redux-toolkit-example/src/router/AppRouter.jsx
@@ -3,13 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import News from "../pages/News";
 import Login from "../pages/Login";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<News />} />
+        <Route path="/" element={<PrivateRouter />}>
+          <Route path="/" element={<News />} />
+        </Route>
+
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
