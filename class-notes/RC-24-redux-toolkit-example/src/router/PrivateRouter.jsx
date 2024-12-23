5478ed50df44366af741ddbb30e49685f49571a1
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-23 17:23:34 +0100

    233.commit

--- class-notes/RC-24-redux-toolkit-example/src/router/PrivateRouter.jsx
+++ class-notes/RC-24-redux-toolkit-example/src/router/PrivateRouter.jsx
@@ -5,6 +5,8 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRouter = () => {
   const { email, password } = useSelector((state) => state.yetkiSlice);
 
+  //? yetkiSlice sayfasından kullanici dolu gelirse ve email=osman ise yönlendirilen sayfaya git
+
   return email === "osman" && password === "1234" ? (
     <Outlet />
   ) : (
