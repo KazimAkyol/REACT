4d7a15c760932a74095cefe55a871a5936c71a8e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-16 12:06:38 +0200

    313.commit

--- class-notes/RC-24-30-Stock-App/session2-starter/src/components/RegisterForm.jsx
+++ class-notes/RC-24-30-Stock-App/session2-starter/src/components/RegisterForm.jsx
@@ -1,31 +1,5 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import { Form } from "formik";
-import * as Yup from "yup";
-
-// Yup ile istediğimiz alanlara istediğimiz validasyon koşullarını oluşturuyoruz.Sonra oluşturduğumuz bu şemayı formike tanımlayarak kullanıyoruz.Böylelikle formik hem formumuzu yönetiyor hem de verdiğimiz validationSchema'yı uyguluyor. Dikkat edilmesi gereken husus; formikte tanımladığımız initialValues daki keylerle, Yupta tanımladığımız keylerin aynı olması.Eğer bir harf bile farklı olsa o alanla ilgili yazdığınız validation çalışmaz.
-export const SignupSchema = Yup.object().shape({
-    username: Yup.string()
-        .required("Bu alan zorunludur!")
-        .min(3, "Username en az 3 karakter olmalıdır!"),
-    firstName: Yup.string()
-        .min(2, "Too Short!")
-        .max(50, "Too Long!")
-        .required("Required"),
-    lastName: Yup.string()
-        .min(2, "Too Short!")
-        .max(50, "Too Long!")
-        .required("Required"),
-    email: Yup.string().email("Invalid email").required("Required"),
-    password: Yup.string()
-        .required()
-        .min(8)
-        .matches(/\d+/, "Digit karakter içermelidir!")
-        .matches(/[a-z]/, "Küçük harf içermelidir!")
-        .matches(/[A-Z]/, "Büyük harf içermelidir!")
-        .matches(/[@$?!%&*]+/, "Özel karakter içermelidir(@$?!%&*)")
-});
+import React from "react";
+import { Button, TextField } from "@mui/material";
 
 const SignUpForm = ({
     values,
@@ -36,75 +10,86 @@ const SignUpForm = ({
     isSubmitting,
 }) => {
     return (
-        <div>
-            <Form>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                    <TextField
-                        name="username" // formik name attributed'ından eşleştirme yapıyor.
-                        label="Username"
-                        value={values.username}
-                        onChange={handleChange}
-                        onBlur={handleBlur} // kullanıcının input alanından ayrıldığını yakalayan event
-                        helperText={touched.username && errors.username} // validation'da verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için errors dan gelen mesajı yakalıyoruz.
-                        error={touched.username && Boolean(errors.username)} //validation'da verdiğimiz kalıba uymazsa rengi errora çevirmesi için error attribute'ı benden false/true degeri bekliyor ondan dolayı daha sağlıklı olması için Boolean deger döndürüyoruz.
-                    // touched'da kullanıcının inputa tıklayıp tıklamadığını yakalıyor
-                    />
-                    <TextField
-                        label="First Name"
-                        name="firstName"
-                        type="text"
-                        variant="outlined"
-                        value={values.firstName}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        helperText={touched.firstName && errors.firstName}
-                        error={touched.firstName && Boolean(errors.firstName)}
-                    />
-                    <TextField
-                        label="Last Name"
-                        name="lastName"
-                        type="text"
-                        variant="outlined"
-                        value={values.lastName}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        helperText={touched.lastName && errors.lastName}
-                        error={touched.lastName && Boolean(errors.lastName)}
-                    />
-                    <TextField
-                        label="Email"
-                        name="email"
-                        type="email"
-                        variant="outlined"
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        helperText={touched.email && errors.email}
-                        error={touched.email && Boolean(errors.email)}
-                    />
-                    <TextField
-                        label="Password"
-                        name="password"
-                        id="password"
-                        type="password"
-                        variant="outlined"
-                        value={values.password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        helperText={touched.password && errors.password}
-                        error={touched.password && Boolean(errors.password)}
-                    />
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Username"
+                name="username" // formik name attributed'ından eşleştirme yapıyor.
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur} // kullanıcının input alanından ayrıldığını yakalayan event
+                helperText={touched.username && errors.username} // validation'da verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için errors dan gelen mesajı yakalıyoruz.
+                error={touched.username && Boolean(errors.username)} //validation'da verdiğimiz kalıba uymazsa rengi errora çevirmesi için error attribute'ı benden false/true degeri bekliyor ondan dolayı daha sağlıklı olması için Boolean deger döndürüyoruz.
+                // touched'da kullanıcının inputa tıklayıp tıklamadığını yakalıyor.
+                variant="outlined"
+                fullWidth
+                type="text"
+                margin="normal"
+            />
 
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        disabled={isSubmitting}
-                    >
-                        {isSubmitting ? "Loading..." : "Sign Up"}
-                    </Button>
-                </Box>
-            </Form>
-        </div>
+            <TextField
+                label="First Name"
+                name="firstName"
+                type="text"
+                variant="outlined"
+                value={values.firstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={touched.firstName && errors.firstName}
+                error={touched.firstName && Boolean(errors.firstName)}
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                label="Last Name"
+                name="lastName"
+                type="text"
+                variant="outlined"
+                value={values.lastName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={touched.lastName && errors.lastName}
+                error={touched.lastName && Boolean(errors.lastName)}
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                label="Email"
+                name="email"
+                type="email"
+                variant="outlined"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={touched.email && errors.email}
+                error={touched.email && Boolean(errors.email)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Password"
+                name="password"
+                id="password"
+                type="password"
+                variant="outlined"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                helperText={touched.password && errors.password}
+                error={touched.password && Boolean(errors.password)}
+                fullWidth
+                margin="normal"
+            />
+
+            <Button
+                type="submit"
+                variant="contained"
+                disabled={isSubmitting}
+            >
+                {isSubmitting ? "Loading..." : "Sign Up"}
+            </Button>
+        </form>
     );
 };
 
--- class-notes/RC-24-30-Stock-App/session2-starter/src/pages/Register.jsx
+++ class-notes/RC-24-30-Stock-App/session2-starter/src/pages/Register.jsx
@@ -8,71 +8,101 @@ import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
-import { Formik} from "formik";
-import useAuthCall from "../hooks/useAuthCall";
-import RegisterForm, { SignupSchema } from "../components/RegisterForm";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import useAuthCall from "../hook/useAuthCall";
+import RegisterForm from "../components/RegisterForm";
 
 const Register = () => {
-  const {register} = useAuthCall()
-  return (
-    <Container maxWidth="lg">
-      <Grid
-        container
-        justifyContent="center"
-        direction="row-reverse"
-        rowSpacing={{ sm: 3 }}
-        sx={{
-          height: "100vh",
-          p: 2,
-        }}
-      >
-        <AuthHeader />
+    const { register } = useAuthCall()
 
-        <Grid item xs={12} sm={10} md={6}>
-          <Avatar
-            sx={{
-              backgroundColor: "secondary.light",
-              m: "auto",
-              width: 40,
-              height: 40,
-            }}
-          >
-            <LockIcon size="30" />
-          </Avatar>
-          <Typography
-            variant="h4"
-            align="center"
-            mb={2}
-            color="secondary.light"
-          >
-            Register
-          </Typography>
+    // Yup ile istediğimiz alanlara istediğimiz validasyon koşullarını oluşturuyoruz.Sonra oluşturduğumuz bu şemayı formike tanımlayarak kullanıyoruz.Böylelikle formik hem formumuzu yönetiyor hem de verdiğimiz validationSchema'yı uyguluyor. Dikkat edilmesi gereken husus; formikte tanımladığımız initialValues daki keylerle, Yupta tanımladığımız keylerin aynı olması.Eğer bir harf bile farklı olsa o alanla ilgili yazdığınız validation çalışmaz.
 
-          <Formik
-            initialValues={{
-              username: "",
-              firstName: "",
-              lastName: "",
-              email: "",
-              password: "",
-            }}
-            validationSchema={SignupSchema}
-            onSubmit={(values, actions) => {
-              register(values);
-              actions.resetForm();// formu temizleme yapar.
-              actions.setSubmitting(false);// isSubmitting değerini tekrar false a çeker
-            }}
-            component={(props) => <RegisterForm {...props} />} //* formikin bize verdiğim metotlar,değişkenler props içersinde geliyor biz de onları RegisterForm componentine göndermiş olduk.
-          ></Formik>
-          <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
-            <Link to="/">Already have an account? Sign in</Link>
-          </Box>
-        </Grid>
+    const SignupSchema = Yup.object().shape({
+        username: Yup.string()
+            .required("Bu alan zorunludur!")
+            .min(3, "Username en az 3 karakter olmalıdır!"),
+        firstName: Yup.string()
+            .min(2, "Too Short!")
+            .max(50, "Too Long!")
+            .required("Required"),
+        lastName: Yup.string()
+            .min(2, "Too Short!")
+            .max(50, "Too Long!")
+            .required("Required"),
+        email: Yup.string().email("Invalid email").required("Required"),
+        password: Yup.string()
+            .required()
+            .min(8)
+            .matches(/\d+/, "Digit karakter içermelidir!")
+            .matches(/[a-z]/, "Küçük harf içermelidir!")
+            .matches(/[A-Z]/, "Büyük harf içermelidir!")
+            .matches(/[@$?!%&*]+/, "Özel karakter içermelidir(@$?!%&*)")
+    });
 
-        <AuthImage image={image} />
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container maxWidth="lg">
+            <Grid
+                container
+                justifyContent="center"
+                direction="row-reverse"
+                rowSpacing={{ sm: 3 }}
+                sx={{
+                    height: "100vh",
+                    p: 2,
+                }}
+            >
+                <AuthHeader />
+
+                <Grid item xs={12} sm={10} md={6}>
+                    <Avatar
+                        sx={{
+                            backgroundColor: "secondary.light",
+                            m: "auto",
+                            width: 40,
+                            height: 40,
+                        }}
+                    >
+                        <LockIcon size="30" />
+                    </Avatar>
+                    <Typography
+                        variant="h4"
+                        align="center"
+                        mb={2}
+                        color="secondary.light"
+                    >
+                        Register
+                    </Typography>
+                    {/* /* -------------------------------------------------------------------------- */}
+                    {/* FORMİK YAPISI */}
+
+                    <Formik
+                        initialValues={{
+                            username: "",
+                            firstName: "",
+                            lastName: "",
+                            email: "",
+                            password: "",
+                        }}
+                        validationSchema={SignupSchema}
+                        onSubmit={(values, actions) => {
+                            register(values);
+                            // action degeri formik'ten gelen metodlari icerir, resetForm ve setSubmitting gibi.
+                            actions.resetForm();// formu temizleme yapar.
+                            actions.setSubmitting(false);// isSubmitting değerini tekrar false a çeker
+                        }}
+                        component={(props) => <RegisterForm {...props} />} //* formikin bize verdiğim metotlar,değişkenler props içersinde geliyor biz de onları RegisterForm componentine göndermiş olduk.
+                    />
+                    {/* /* -------------------------------------------------------------------------- */}
+                    <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
+                        <Link to="/">Already have an account? Sign in</Link>
+                    </Box>
+                </Grid>
+
+                <AuthImage image={image} />
+            </Grid>
+        </Container>
+    );
 };
 
 export default Register;
