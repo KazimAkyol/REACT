acfa89699b862e093f1ddc55014c18e870dccf6e
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-20 21:04:02 +0200

    316.commit

--- class-notes/RC-14-Shopping-Cart/starter/src/components/ProductCard.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/components/ProductCard.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import axios from "axios";
 
-const ProductCard = () => {
+const ProductCard = ({ ürün, getUrunler }) => {
+    const remove = async () => {
+        await axios.delete(
+            `https://63f4e5583f99f5855db9e941.mockapi.io/products/${ürün.id}`
+        );
+
+        getUrunler();
+    };
+
+    const arttir = async () => {
+        await axios.put(
+            `https://63f4e5583f99f5855db9e941.mockapi.io/products/${ürün.id}`,
+            { ...ürün, amount: ürün.amount + 1 }
+        );
+        getUrunler();
+    };
+
+    const azalt = async () => {
+        await axios.put(
+            `https://63f4e5583f99f5855db9e941.mockapi.io/products/${ürün.id}`,
+            { ...ürün, amount: ürün.amount - 1 }
+        );
+        getUrunler();
+    };
+
     return (
         <div className="card shadow-lg mb-3">
             <div className="row g-0">
                 <div className="col-md-5">
                     <img
-                        src={"image"}
+                        src={ürün.image}
                         className="w-100 h-100 rounded-start"
                         alt={"name"}
                         title={""}
@@ -15,34 +39,43 @@ const ProductCard = () => {
                 <div className="col-md-7">
                     <div className="card-body">
                         <h5 className="card-title" role="button">
-                            Ürün İsmi
+                            {ürün.name}
                         </h5>
                         <div className="product-price d-flex flex-wrap align-items-center">
-                            <span className="damping-price text-warning h2">$Fiyat</span>
+                            <span className="damping-price text-warning h2">
+                                ${(ürün.price * ürün.dampingRate).toFixed(2)}
+                            </span>
+
                             <span className="h5 text-dark text-decoration-line-through">
-                                Asıl Fiyat
+                                {ürün.price}
                             </span>
                         </div>
                         <div className="border border-1 border-dark shadow-lg d-flex justify-content-center p-2">
                             <div className="quantity-controller">
-                                <button className="btn btn-secondary btn-sm">
+                                <button onClick={azalt} className="btn btn-secondary btn-sm">
                                     <i className="fas fa-minus"></i>
                                 </button>
                                 <p className="d-inline mx-4" id="product-quantity">
-                                    Miktar
+                                    {ürün.amount}
                                 </p>
-                                <button className="btn btn-secondary btn-sm">
+                                <button onClick={arttir} className="btn btn-secondary btn-sm">
                                     <i className="fas fa-plus"></i>
                                 </button>
                             </div>
                         </div>
                         <div className="product-removal mt-4">
-                            <button className="btn btn-danger btn-sm w-100 remove-product">
+                            <button
+                                onClick={remove}
+                                className="btn btn-danger btn-sm w-100 remove-product"
+                            >
                                 <i className="fa-solid fa-trash-can me-2"></i>Remove
                             </button>
                         </div>
                         <div className="mt-2">
-                            Product Total: $<span className="product-line-price">Total</span>
+                            Product Total: $
+                            <span className="product-line-price">
+                                {ürün.price * ürün.dampingRate * ürün.amount}{" "}
+                            </span>
                         </div>
                     </div>
                 </div>
--- class-notes/RC-14-Shopping-Cart/starter/src/pages/ProductList.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/pages/ProductList.jsx
@@ -1,23 +1,46 @@
-import React from "react";
-import ProductCard from "../components/ProductCard";
+import React, { useEffect, useState } from "react";
 import CardTotal from "../components/CardTotal";
-
+import axios from "axios";
+import ProductCard from "../components/ProductCard";
 const ProductList = () => {
+    const BASE_URL = "https://63f4e5583f99f5855db9e941.mockapi.io/products";
+
+    const [produkte, setProdukte] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getUrunler = async () => {
+        const { data } = await axios.get(BASE_URL);
+
+        // console.log(data);
+
+        setProdukte(data);
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getUrunler();
+    }, []);
+
     return (
         <div className="container mt-3">
-            <div className={"bg-light d-sm-block d-md-flex"}>
-                <p className="text-center text-danger w-100">Loading...</p>
+            <div className="bg-light d-sm-block d-md-flex">
+                {loading === true ? (
+                    <p className="text-center text-danger w-100">Loading....</p>
+                ) : (
+                    <>
+                        <article id="product-panel" className="col-md-6">
+                            {produkte.map((ürün) => (
+                                <ProductCard ürün={ürün} getUrunler={getUrunler} />
+                            ))}
+                        </article>
+                        <article className="col-md-4 m-3">
+                            <CardTotal produkte={produkte} />
+                        </article>
+                    </>
+                )}
 
-                <>
-                    <article id="product-panel" className="col-md-6">
-                        <ProductCard />
-                    </article>
-                    <article className="col-md-4 m-3">
-                        <CardTotal />
-                    </article>
-                </>
 
-                <p className="text-center text-danger w-100">No products data...</p>
             </div>
         </div>
     );
