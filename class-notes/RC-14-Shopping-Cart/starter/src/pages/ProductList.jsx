e0d83e611eda32e5cdd5c5d5b1be6f1bc0d5a031
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-23 00:18:23 +0200

    320.commit

--- class-notes/RC-14-Shopping-Cart/starter/src/pages/ProductList.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/pages/ProductList.jsx
@@ -40,8 +40,6 @@ const ProductList = () => {
                         </article>
                     </>
                 )}
-
-
             </div>
         </div>
     );
--- class-notes/RC-14-Shopping-Cart/starter/src/router/AppRouter.jsx
+++ class-notes/RC-14-Shopping-Cart/starter/src/router/AppRouter.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Main from "../pages/Main";
-import About from "../pages/About";
-import NewProduct from "../pages/NewProduct";
-import ProductList from "../pages/ProductList";
-import UpdateProduct from "../pages/UpdateProduct";
+import React from 'react'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Main from '../pages/Main'
+import NewProduct from '../pages/NewProduct'
+import ProductList from '../pages/ProductList'
+import About from '../pages/About'
 
 const AppRouter = () => {
     return (
-        <Router>
+        <BrowserRouter>
             <Navbar />
             <Routes>
-                <Route exact path="/" element={<Main />} />
-                <Route path="/newProduct" element={<NewProduct />} />
-                <Route path="/productList" element={<ProductList />} />
-                <Route path="/about" element={<About />} />
+                <Route exact path='/' element={<Main />} />
+                <Route path='/new-product' element={<NewProduct />} />
+                <Route path='/products' element={<ProductList />} />
+                <Route path='/about' element={<About />} />
             </Routes>
-            <UpdateProduct />
-        </Router>
-    );
-};
+        </BrowserRouter>
+    )
+}
 
-export default AppRouter;
+export default AppRouter
