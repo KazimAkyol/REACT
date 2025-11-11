34008e9742f3da7ec1f8dd0e200a37ecce0cf3c7
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-11-11 22:54:55 +0100

    328.commit

--- class-notes/RC-24-30-Stock-App/inclass-formik-yup/src/components/BrandCard.jsx
+++ class-notes/RC-24-30-Stock-App/inclass-formik-yup/src/components/BrandCard.jsx
@@ -12,123 +12,123 @@ import EditIcon from "@mui/icons-material/Edit";
 import useStockCall from "../hook/useStockCall";
 
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
 })(({ theme }) => ({
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-  variants: [
-    {
-      props: ({ expand }) => !expand,
-      style: {
-        transform: "rotate(0deg)",
-      },
-    },
-    {
-      props: ({ expand }) => !!expand,
-      style: {
-        transform: "rotate(180deg)",
-      },
-    },
-  ],
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+    }),
+    variants: [
+        {
+            props: ({ expand }) => !expand,
+            style: {
+                transform: "rotate(0deg)",
+            },
+        },
+        {
+            props: ({ expand }) => !!expand,
+            style: {
+                transform: "rotate(180deg)",
+            },
+        },
+    ],
 }));
 
 export default function BrandCard({
-  _id,
-  name,
-  phone,
-  address,
-  image,
-  handleOpen,
-  setInitialState,
+    _id,
+    name,
+    phone,
+    address,
+    image,
+    handleOpen,
+    setInitialState,
 }) {
-  const [expanded, setExpanded] = React.useState(false);
+    const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
-  /* -------------------------------------------------------------------------- */
-  const { getDeleteData } = useStockCall();
-  /* -------------------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------- */
+    const { getDeleteData } = useStockCall();
+    /* -------------------------------------------------------------------------- */
 
-  return (
-    <Card
-      sx={{
-        minWidth: 340,
-        maxWidth: 345,
-        height: 420, // Kart yüksekliği sabit
-        display: "flex",
-        flexDirection: "column", // İçerikleri dikeyde hizala
-        justifyContent: "space-between", // Kartın altındaki ikonları sabit tut
-        marginBottom: "1rem",
-        marginRight: "1rem",
-        boxShadow: "1 0 5px gray",
-      }}
-    >
-      <CardHeader
-        title={name}
-        subheader={address}
-        sx={{
-          display: "-webkit-box",
-          WebkitBoxOrient: "vertical",
-          WebkitLineClamp: 3, // Maksimum 3 satır göster
-          overflow: "hidden", // Fazlalıkları gizle
-        }}
-      />
-      <CardMedia
-        component="img"
-        height="150"
-        image={image}
-        alt="Paella dish"
-        sx={{ padding: "2rem", objectFit: "scale-down" }}
-      />
-      <CardContent
-        sx={{
-          flexGrow: 1, // Kartların içerik alanlarını eşit boyutta yap
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          variant="body2"
-          sx={{ color: "text.secondary", textAlign: "center" }}
-        >
-          {phone}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-          aria-label="add to favorites"
-          onClick={() => getDeleteData("brands", _id)}
-        >
-          <DeleteOutlineIcon
+    return (
+        <Card
             sx={{
-              "&:hover": {
-                color: "red",
-              },
+                minWidth: 340,
+                maxWidth: 345,
+                height: 420, // Kart yüksekliği sabit
+                display: "flex",
+                flexDirection: "column", // İçerikleri dikeyde hizala
+                justifyContent: "space-between", // Kartın altındaki ikonları sabit tut
+                marginBottom: "1rem",
+                marginRight: "1rem",
+                boxShadow: "1 0 5px gray",
             }}
-          />
-        </IconButton>
-        <IconButton
-          aria-label="share"
-          onClick={() => {
-            handleOpen();
-            setInitialState({ _id, name, image });
-          }}
         >
-          <EditIcon
-            sx={{
-              "&:hover": {
-                color: "red",
-              },
-            }}
-          />
-        </IconButton>
-      </CardActions>
-    </Card>
-  );
+            <CardHeader
+                title={name}
+                subheader={address}
+                sx={{
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 3, // Maksimum 3 satır göster
+                    overflow: "hidden", // Fazlalıkları gizle
+                }}
+            />
+            <CardMedia
+                component="img"
+                height="150"
+                image={image}
+                alt="Paella dish"
+                sx={{ padding: "2rem", objectFit: "scale-down" }}
+            />
+            <CardContent
+                sx={{
+                    flexGrow: 1, // Kartların içerik alanlarını eşit boyutta yap
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    variant="body2"
+                    sx={{ color: "text.secondary", textAlign: "center" }}
+                >
+                    {phone}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton
+                    aria-label="add to favorites"
+                    onClick={() => getDeleteData("brands", _id)}
+                >
+                    <DeleteOutlineIcon
+                        sx={{
+                            "&:hover": {
+                                color: "red",
+                            },
+                        }}
+                    />
+                </IconButton>
+                <IconButton
+                    aria-label="share"
+                    onClick={() => {
+                        handleOpen();
+                        setInitialState({ _id, name, image });
+                    }}
+                >
+                    <EditIcon
+                        sx={{
+                            "&:hover": {
+                                color: "red",
+                            },
+                        }}
+                    />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
 }
--- class-notes/RC-24-30-Stock-App/inclass-formik-yup/src/components/Modals/BrandModal.jsx
+++ class-notes/RC-24-30-Stock-App/inclass-formik-yup/src/components/Modals/BrandModal.jsx
@@ -7,84 +7,84 @@ import { useState } from "react";
 import useStockCall from "../../hook/useStockCall";
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
 };
 
 export default function BrandModal({ open, handleClose, initialState }) {
-  const { postStockData, putStockData } = useStockCall();
+    const { postStockData, putStockData } = useStockCall();
 
-  const [info, setInfo] = useState(initialState);
+    const [info, setInfo] = useState(initialState);
 
-  const handleChange = (e) => {
-    setInfo({ ...info, [e.target.name]: e.target.value });
-  };
+    const handleChange = (e) => {
+        setInfo({ ...info, [e.target.name]: e.target.value });
+    };
 
-  const handleSubmit = () => {
-    // Database info bilgisini gönderme işlemi
-    if (info._id) {
-      putStockData("brands", info);
-    } else {
-      postStockData("brands", info);
-    }
-  };
+    const handleSubmit = () => {
+        // Database info bilgisini gönderme işlemi
+        if (info._id) {
+            putStockData("brands", info);
+        } else {
+            postStockData("brands", info);
+        }
+    };
 
-  // useEffect didUpdate metodu tarzında çalışması. dependancy arrayde başlangıç değeri verildiğinde güncelleme yapması.
-  // useEffect(()=>{setInfo(initialState)},[initialState])
+    // useEffect didUpdate metodu tarzında çalışması. dependancy arrayde başlangıç değeri verildiğinde güncelleme yapması.
+    // useEffect(()=>{setInfo(initialState)},[initialState])
 
-  return (
-    <div>
-      {/* Button modalı açıyor.Açma işlemi brands sayfasında NEw Brand yaptığı için burda pasif hale getirdik */}
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-          >
-            <TextField
-              label="Brand Name"
-              variant="outlined"
-              type="text"
-              name="name"
-              onChange={handleChange}
-              value={info.name}
-            />
-
-            <TextField
-              label="Image Url"
-              variant="outlined"
-              type="text"
-              name="image"
-              onChange={handleChange}
-              value={info.image}
-            />
-            <Button
-              type="submit"
-              sx={{
-                backgroundColor: "secondary.main",
-                color: "white",
-                "&:hover": { backgroundColor: "secondary.main" },
-              }}
+    return (
+        <div>
+            {/* Button modalı açıyor.Açma işlemi brands sayfasında NEw Brand yaptığı için burda pasif hale getirdik */}
+            {/* <Button onClick={handleOpen}>Open modal</Button> */}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
             >
-              {info._id ? "UPDATE" : "ADD FIRM"}
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
-    </div>
-  );
+                <Box sx={style}>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+                    >
+                        <TextField
+                            label="Brand Name"
+                            variant="outlined"
+                            type="text"
+                            name="name"
+                            onChange={handleChange}
+                            value={info.name}
+                        />
+
+                        <TextField
+                            label="Image Url"
+                            variant="outlined"
+                            type="text"
+                            name="image"
+                            onChange={handleChange}
+                            value={info.image}
+                        />
+                        <Button
+                            type="submit"
+                            sx={{
+                                backgroundColor: "secondary.main",
+                                color: "white",
+                                "&:hover": { backgroundColor: "secondary.main" },
+                            }}
+                        >
+                            {info._id ? "UPDATE" : "ADD FIRM"}
+                        </Button>
+                    </Box>
+                </Box>
+            </Modal>
+        </div>
+    );
 }
